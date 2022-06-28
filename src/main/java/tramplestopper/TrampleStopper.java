aa940256fe69b50789ee8f4c1a8a2c0c656e3b5b
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-06-28 09:17:48 -0500

    Updates

--- src/main/java/tramplestopper/TrampleStopper.java
+++ src/main/java/tramplestopper/TrampleStopper.java
@@ -3,6 +3,7 @@ package tramplestopper;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.StatType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,9 +15,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tramplestopper.test.GameTestHandler;
+
+import java.util.function.Supplier;
 
 @Mod(TrampleStopper.MOD_ID)
 @Mod.EventBusSubscriber
@@ -27,9 +33,11 @@ public class TrampleStopper
     public static ForgeConfigSpec.EnumValue<TrampleType> type;
     public static ForgeConfigSpec.IntValue intValue;
     public static ForgeConfigSpec.DoubleValue doubleValue;
-    public static ResourceLocation FARMLAND_TRAMPLED;
-    public static ResourceLocation FARMLAND_NOT_TRAMPLED;
+    public static RegistryObject<ResourceLocation> FARMLAND_TRAMPLED;
+    public static RegistryObject<ResourceLocation> FARMLAND_NOT_TRAMPLED;
     private static TrampleStopper THIS;
+
+    private static final DeferredRegister<ResourceLocation> STATS = DeferredRegister.create(Registry.CUSTOM_STAT_REGISTRY, MOD_ID);
 
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -49,9 +57,10 @@ public class TrampleStopper
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, builder.build());
         doubleValue = builder.comment("At how many blocks should trampling stop start")
                 .defineInRange("blocks",  0.0, 0, Short.MAX_VALUE);
+        FARMLAND_TRAMPLED = STATS.register("farmland_trampled", () -> new ResourceLocation(MOD_ID, "farmland_trampled"));
+        FARMLAND_NOT_TRAMPLED = STATS.register("farmland_not_trampled", () -> new ResourceLocation(MOD_ID, "farmland_not_trampled"));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.addListener(TrampleStopper::onCropTrample);
-
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerGameTest);
     }
 
@@ -62,8 +71,6 @@ public class TrampleStopper
 
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Hello");
-        FARMLAND_TRAMPLED = Registry.register(Registry.CUSTOM_STAT, new ResourceLocation(MOD_ID, "farmland_trampled"), new ResourceLocation(MOD_ID, "farmland_trampled"));
-        FARMLAND_NOT_TRAMPLED = Registry.register(Registry.CUSTOM_STAT, new ResourceLocation(MOD_ID, "farmland_saved"), new ResourceLocation(MOD_ID, "farmland_saved"));
     }
 
     @SubscribeEvent
@@ -72,7 +79,7 @@ public class TrampleStopper
             event.setCanceled(true);
         }
         if(event.getEntity() instanceof Player player) {
-            player.awardStat(event.isCanceled() ? FARMLAND_NOT_TRAMPLED : FARMLAND_TRAMPLED);
+            player.awardStat(event.isCanceled() ? FARMLAND_NOT_TRAMPLED.get() : FARMLAND_TRAMPLED.get());
         }
     }
 }
