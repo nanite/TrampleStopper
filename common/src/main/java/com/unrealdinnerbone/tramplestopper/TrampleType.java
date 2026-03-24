a4adfcf2807d2f65fbbe593f9b132f27f10313ef
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2026-03-24 18:31:23 -0500

    Update to 26.1

--- common/src/main/java/com/unrealdinnerbone/tramplestopper/TrampleType.java
+++ common/src/main/java/com/unrealdinnerbone/tramplestopper/TrampleType.java
@@ -4,37 +4,35 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 
-import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public enum TrampleType {
 
     FEATHER_FALLING((entity) -> {
         if (entity instanceof LivingEntity livingEntity) {
-                for (ItemStack itemStack : livingEntity.getArmorSlots()) {
-                    if (itemStack.getItem() instanceof ArmorItem armorItem) {
-                        if(armorItem.getEquipmentSlot() == EquipmentSlot.FEET) {
-                            RegistryAccess.Frozen frozen = livingEntity.getServer().registryAccess();
-                            HolderLookup.RegistryLookup<Enchantment> lookup = frozen.lookupOrThrow(Registries.ENCHANTMENT);
-                            if (EnchantmentHelper.getItemEnchantmentLevel(lookup.getOrThrow(Enchantments.FEATHER_FALLING), itemStack) >= TrampleConfig.CONFIG.get().featherFallingLevel()) {
-                                return true;
-                            }
-                        }
-                    }
+            RegistryAccess frozen = livingEntity.registryAccess();
+            HolderLookup.RegistryLookup<Enchantment> lookup = frozen.lookupOrThrow(Registries.ENCHANTMENT);
+            Holder.Reference<Enchantment> featherFalling = lookup.getOrThrow(Enchantments.FEATHER_FALLING);
+            ItemStack stack = livingEntity.getItemBySlot(EquipmentSlot.FEET);
+            if (stack.has(DataComponents.ENCHANTMENTS)) {
+                ItemEnchantments itemEnchantments = stack.get(DataComponents.ENCHANTMENTS);
+                if (itemEnchantments == null) {
+                    return false;
                 }
+                int level = itemEnchantments.getLevel(featherFalling);
+                return level >= TrampleConfig.CONFIG.get().featherFallingLevel();
+            }
         }
         return false;
     }),
--- fabric/src/main/java/com/unrealdinnerbone/tramplestopper/mixin/FarmBlockMixin.java
+++ fabric/src/main/java/com/unrealdinnerbone/tramplestopper/mixin/FarmBlockMixin.java
@@ -4,18 +4,18 @@ import com.unrealdinnerbone.tramplestopper.TrampleConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.FarmlandBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FarmBlock.class)
+@Mixin(FarmlandBlock.class)
 public class FarmBlockMixin {
 
     @Inject(method = "fallOn", at = @At("HEAD"), cancellable = true)
-    public void onLandedUpon(Level level, BlockState blockState, BlockPos blockPos, Entity entity, float f, CallbackInfo ci) {
+    public void onLandedUpon(Level level, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
         if (TrampleConfig.CONFIG.get().type().getFunction().apply(entity)) {
             ci.cancel();
         }
